b5ce44c99ac1ae57398495cdadddd461896ab8df
Jhony279 <lordjhony732@gmail.comm>
2026-04-21 23:36:53 -0700

    Creating deque and starting it off with 2 car objects

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 // COMSC-210 | Lab 32 | Johnathan Perez Baltazar
 
 #include <iostream>
+#include <deque>
+#include "Car.h"
 using namespace std;
 
 // Function Prototypes:
@@ -10,5 +12,13 @@ using namespace std;
  * @return 
 */
 int main() {
+    deque<Car> d = {Car(), Car()};
+
+    // Print the initial deque
+    cout << "Initial Deque:\n";
+    for (Car c : d) {
+        c.print();
+    }
+
     return 0;
 }
