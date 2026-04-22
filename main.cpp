cb2255de7ac07ebbde5bd1eae072a3a3636d25b3
Jhony279 <lordjhony732@gmail.comm>
2026-04-21 23:56:50 -0700

    Using while loop to iterate through the deque container

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ using namespace std;
 // Function Prototypes:
 
 const int INITIAL_SIZE = 2;
+const int PAY_PROB = 55, JOIN_PROB = 45;
 
 /**
  * @brief - Description of main
@@ -17,15 +18,28 @@ int main() {
     srand(time(0)); // Seed the random number generator
 
     // Create a deque and populate it with Car objects
-    deque<Car> d;
+    deque<Car> tollLine;
     for (int i = 0; i < INITIAL_SIZE; i++) {
-        d.push_back(Car());
+        tollLine.push_back(Car());
     }
 
     // Print the initial deque
     cout << "Initial Deque:\n";
-    for (Car c : d) {
+    for (Car c : tollLine) {
         c.print();
+    }
+
+    while(!tollLine.empty()){
+        // Generate a random number between 0 and 99
+        int p, j = rand() % 100;
+
+        if (p < PAY_PROB) {
+            tollLine.pop_front(); // Remove the front car from the deque
+        }
+        if (j < JOIN_PROB) {
+            tollLine.push_back(Car()); // Add a new car to the back of the deque
+        }
+        
     }
 
     return 0;
