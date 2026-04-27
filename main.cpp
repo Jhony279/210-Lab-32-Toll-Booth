a0f7ac7bf82e9937acb633cb891460ebc5702cd4
Jhony279 <lordjhony732@gmail.com>
2026-04-26 18:21:17 -0700

    Creating print function

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@ using namespace std;
 const int INITIAL_SIZE = 2;
 const int PAY_PROB = 55, JOIN_PROB = 45;
 
+void printQueue(const deque<Car>&);
+
 /**
  * @brief Simulation of a toll booth by using a deque to manage the queue of cars.
  * @return 
@@ -33,32 +35,42 @@ int main() {
         }
     }
 
-    // int i = 1; // Counter for the number of iterations
-    // while(!tollLine.empty()){
-    //     int p = rand() % 100; // Generate a random number between 0 and 99
+    int i = 1; // Counter for the number of iterations
+    while(!tollLine.empty()){
+        int p = rand() % 100; // Generate a random number between 0 and 99
 
-    //     cout << "Time: " << i << " Operation: ";
-    //     if (p < PAY_PROB) {
-    //         cout << "Car Paid: ";
-    //         tollLine.front().print(); // Print the front car that is paying
-    //         tollLine.pop_front(); // Remove the front car from the deque
-    //         ++i;
+        cout << "Time: " << i << " Operation: ";
+        if (p < PAY_PROB) {
+            cout << "Car Paid: ";
+            tollLine.front().print(); // Print the front car that is paying
+            tollLine.pop_front(); // Remove the front car from the deque
+            ++i;
 
-    //     } else {
-    //         cout << "Joined Lane: ";
-    //         tollLine.back().print(); // Print the front car that is paying
-    //         tollLine.push_back(Car()); // Add a new car to the back of the deque
-    //         ++i;
-    //     }
+        } else {
+            cout << "Joined Lane: ";
+            tollLine.back().print(); // Print the front car that is paying
+            tollLine.push_back(Car()); // Add a new car to the back of the deque
+            ++i;
+        }
 
-    //     // Print remaining cars in the queue
-    //     cout << "queue:\n";
-    //     for (Car c : tollLine) {
-    //         cout << "   ";
-    //         c.print();
-    //     }
-    // }
-    // cout << "   Empty\n";
+        // Print remaining cars in the queue
+        printQueue(tollLanes);
+    }
+    cout << "   Empty\n";
 
     return 0;
+}
+
+void printQueue(const array<deque<Car>, 4>& tollLanes) {
+    for (int lane = 0; lane < tollLanes.size(); lane++) {
+        cout << "Lane " << lane + 1 << ":\n";
+        if (tollLanes[lane].empty()) {
+            cout << "   Empty\n";
+        } else {
+            for (Car c : tollLanes[lane]) {
+                cout << "   ";
+                c.print();
+            }
+        }
+    }
 }
