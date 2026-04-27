5d6e12e97b1a9075b5f92a2d9293c50097c0195f
Jhony279 <lordjhony732@gmail.com>
2026-04-26 18:06:28 -0700

    Creating an array for lanes and changing code to use new array

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <deque>
 #include "Car.h"
+#include <array>
 using namespace std;
 
 const int INITIAL_SIZE = 2;
@@ -15,44 +16,51 @@ int main() {
     srand(time(0)); // Seed the random number generator
 
     // Create a deque and populate it with Car objects
-    deque<Car> tollLine;
+    // deque<Car> tollLine;
+    array<deque<Car>, 4> tollLanes; // Array of 4 deques for 4 toll lanes
+
     for (int i = 0; i < INITIAL_SIZE; i++) {
-        tollLine.push_back(Car());
-    }
-
-    // Print the initial deque
-    cout << "Initial queue:\n";
-    for (Car c : tollLine) {
-        cout << "   ";
-        c.print();
-    }
-
-    int i = 1; // Counter for the number of iterations
-    while(!tollLine.empty()){
-        int p = rand() % 100; // Generate a random number between 0 and 99
-
-        cout << "Time: " << i << " Operation: ";
-        if (p < PAY_PROB) {
-            cout << "Car Paid: ";
-            tollLine.front().print(); // Print the front car that is paying
-            tollLine.pop_front(); // Remove the front car from the deque
-            ++i;
-
-        } else {
-            cout << "Joined Lane: ";
-            tollLine.back().print(); // Print the front car that is paying
-            tollLine.push_back(Car()); // Add a new car to the back of the deque
-            ++i;
+        for (int lane = 0; lane < tollLanes.size(); lane++) {
+            tollLanes[lane].push_back(Car());
         }
+    }
 
-        // Print remaining cars in the queue
-        cout << "queue:\n";
-        for (Car c : tollLine) {
+    // print the initial state of each toll lane
+    cout << "Initial queue" << endl;
+    for (int lane = 0; lane < tollLanes.size(); lane++) {
+        cout << "Lane " << lane + 1 << ":\n";
+        for (Car c : tollLanes[lane]) {
             cout << "   ";
             c.print();
         }
     }
-    cout << "   Empty\n";
+
+    // int i = 1; // Counter for the number of iterations
+    // while(!tollLine.empty()){
+    //     int p = rand() % 100; // Generate a random number between 0 and 99
+
+    //     cout << "Time: " << i << " Operation: ";
+    //     if (p < PAY_PROB) {
+    //         cout << "Car Paid: ";
+    //         tollLine.front().print(); // Print the front car that is paying
+    //         tollLine.pop_front(); // Remove the front car from the deque
+    //         ++i;
+
+    //     } else {
+    //         cout << "Joined Lane: ";
+    //         tollLine.back().print(); // Print the front car that is paying
+    //         tollLine.push_back(Car()); // Add a new car to the back of the deque
+    //         ++i;
+    //     }
+
+    //     // Print remaining cars in the queue
+    //     cout << "queue:\n";
+    //     for (Car c : tollLine) {
+    //         cout << "   ";
+    //         c.print();
+    //     }
+    // }
+    // cout << "   Empty\n";
 
     return 0;
 }
